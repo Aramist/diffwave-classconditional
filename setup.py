b96f4323ce1cff8b3e2da7548c6dd04478d110bb
Aramis Tanelus <atanelus@flatironinstitute.org>
2024-01-10 09:46:02 -0500

    Run formatter

--- setup.py
+++ setup.py
@@ -16,45 +16,42 @@
 from setuptools import find_packages, setup
 
 
-VERSION = '0.1.8'
-DESCRIPTION = 'diffwave'
-AUTHOR = 'LMNT, Inc.'
-AUTHOR_EMAIL = 'github@lmnt.com'
-URL = 'https://www.lmnt.com'
-LICENSE = 'Apache 2.0'
-KEYWORDS = ['diffwave machine learning neural vocoder tts speech']
+VERSION = "0.1.8"
+DESCRIPTION = "diffwave"
+AUTHOR = "LMNT, Inc."
+AUTHOR_EMAIL = "github@lmnt.com"
+URL = "https://www.lmnt.com"
+LICENSE = "Apache 2.0"
+KEYWORDS = ["diffwave machine learning neural vocoder tts speech"]
 CLASSIFIERS = [
-  'Development Status :: 4 - Beta',
-  'Intended Audience :: Developers',
-  'Intended Audience :: Education',
-  'Intended Audience :: Science/Research',
-  'License :: OSI Approved :: Apache Software License',
-  'Programming Language :: Python :: 3.5',
-  'Programming Language :: Python :: 3.6',
-  'Programming Language :: Python :: 3.7',
-  'Programming Language :: Python :: 3.8',
-  'Topic :: Scientific/Engineering :: Mathematics',
-  'Topic :: Software Development :: Libraries :: Python Modules',
-  'Topic :: Software Development :: Libraries',
+    "Development Status :: 4 - Beta",
+    "Intended Audience :: Developers",
+    "Intended Audience :: Education",
+    "Intended Audience :: Science/Research",
+    "License :: OSI Approved :: Apache Software License",
+    "Programming Language :: Python :: 3.5",
+    "Programming Language :: Python :: 3.6",
+    "Programming Language :: Python :: 3.7",
+    "Programming Language :: Python :: 3.8",
+    "Topic :: Scientific/Engineering :: Mathematics",
+    "Topic :: Software Development :: Libraries :: Python Modules",
+    "Topic :: Software Development :: Libraries",
 ]
 
 
-setup(name = 'diffwave',
-    version = VERSION,
-    description = DESCRIPTION,
-    long_description = open('README.md', 'r').read(),
-    long_description_content_type = 'text/markdown',
-    author = AUTHOR,
-    author_email = AUTHOR_EMAIL,
-    url = URL,
-    license = LICENSE,
-    keywords = KEYWORDS,
-    packages = find_packages('src'),
-    package_dir = { '': 'src' },
-    install_requires = [
-        'numpy',
-        'torch>=1.6',
-        'torchaudio>=0.9.0',
-        'tqdm'
-    ],
-    classifiers = CLASSIFIERS)
+setup(
+    name="diffwave",
+    version=VERSION,
+    description=DESCRIPTION,
+    long_description=open("README.md", "r").read(),
+    long_description_content_type="text/markdown",
+    author=AUTHOR,
+    author_email=AUTHOR_EMAIL,
+    url=URL,
+    license=LICENSE,
+    keywords=KEYWORDS,
+    packages=find_packages("src"),
+    package_dir={"": "src"},
+    install_requires=["numpy", "torch>=1.6", "torchaudio>=0.9.0", "tqdm"],
+    classifiers=CLASSIFIERS,
+)
